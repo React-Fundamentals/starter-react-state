1b29b992c076e301c65983119830b0fcfd8709e1
kchia <kchia87@gmail.com>
2021-09-22 17:00:49 -0400

    add form example

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./Form.module.css";
 export default function Form({
   initialFormData = {
@@ -7,6 +8,23 @@ export default function Form({
     message: "",
   },
 }) {
+  const [formData, setFormData] = useState({});
+
+  // function handleFirstNameChange(event) {
+  //   setFormData((data) => ({ ...data, firstName: event.target.value }));
+  // }
+
+  // function handleLastNameChange(event) {
+  //   setFormData((data) => ({ ...data, lastName: event.target.value }));
+  // }
+
+  function handleChange({ target: { name, value } }) {
+    setFormData((data) => ({
+      ...data,
+      [name]: value,
+    }));
+  }
+
   return (
     <section className={styles.container}>
       <h2>
@@ -17,11 +35,23 @@ export default function Form({
           <ul>
             <li>
               <label htmlFor="firstName">First Name:</label>
-              <input type="text" id="firstName" name="firstName" />
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+              />
             </li>
             <li>
               <label htmlFor="lastName">Last Name:</label>
-              <input type="text" id="lastName" name="lastName" />
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+              />
             </li>
             <li>
               <label htmlFor="email">Email:</label>
