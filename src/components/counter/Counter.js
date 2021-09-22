7d7edce23f7b4f0b602aa7b919bd81d67b840459
kchia <kchia87@gmail.com>
2021-09-22 12:06:51 -0400

    add toggle function

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -7,24 +7,43 @@ export default function Counter({
 }) {
   const [count, setCount] = useState(initialCount);
 
+  // const [name, setName] = useState(initialPerson.name);
+  // const [location, setLocation] = useState(initialPerson.location);
+
+  const [person, setPerson] = useState({ ...initialPerson });
+
   function handleIncrement() {
     setCount((previousCount) => previousCount + 1);
+    // setCount((previousCount) => previousCount + 1);
+    // setCount((previousCount) => previousCount + 1);
+  }
+
+  function handleDecrement() {
+    setCount((previousCount) => previousCount - 1);
+  }
+
+  function handleToggle() {
+    setPerson((previousPerson) =>
+      previousPerson.name === initialPerson.name
+        ? { name: "Jane", location: "SF" }
+        : { ...initialPerson }
+    );
   }
 
   return (
     <section className={styles.container}>
-      <h1>{initialPerson.name}'s Counter App</h1>
+      <h1>{person.name}'s Counter App</h1>
       <h2>Your current count: {count}</h2>
-      <h3>Your current Location: {initialPerson.location}</h3>
+      <h3>Your current Location: {person.location}</h3>
 
       <p>
         When you define a component, a common pattern is for an event handler to
         be a function in the component.
       </p>
       <button onClick={handleIncrement}>increment (+)</button>
-      <button>decrement (-)</button>
+      <button onClick={handleDecrement}>decrement (-)</button>
       <button>reset</button>
-      <button>toggle</button>
+      <button onClick={handleToggle}>toggle</button>
     </section>
   );
 }
