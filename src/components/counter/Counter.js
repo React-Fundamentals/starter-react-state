d6db84d1aeb46b9bc17c9e00aad16b1906d49fe9
kchia <kchia87@gmail.com>
2021-09-22 10:58:22 -0400

    complete review day 2

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,3 +1,23 @@
-export default function Counter() {
-  return <h2>Counter</h2>;
+import styles from "./Counter.module.css";
+
+export default function Counter({
+  initialPerson = { name: "John", location: "NYC" },
+  initialCount = 2,
+}) {
+  return (
+    <section className={styles.container}>
+      <h1>{initialPerson.name}'s Counter App</h1>
+      <h2>Your current count: {initialCount}</h2>
+      <h3>Your current Location: {initialPerson.location}</h3>
+
+      <p>
+        When you define a component, a common pattern is for an event handler to
+        be a function in the component.
+      </p>
+      <button>increment (+)</button>
+      <button>decrement (-)</button>
+      <button>reset</button>
+      <button>toggle</button>
+    </section>
+  );
 }
--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,3 +1,40 @@
-export default function Form() {
-  return <h2>Form</h2>;
+import styles from "./Form.module.css";
+export default function Form({
+  initialFormData = {
+    firstName: "Hou",
+    lastName: "Chia",
+    email: "",
+    message: "",
+  },
+}) {
+  return (
+    <section className={styles.container}>
+      <h2>
+        Contact Form from {initialFormData.firstName} {initialFormData.lastName}
+      </h2>
+      {
+        <form>
+          <ul>
+            <li>
+              <label htmlFor="firstName">First Name:</label>
+              <input type="text" id="firstName" name="firstName" />
+            </li>
+            <li>
+              <label htmlFor="lastName">Last Name:</label>
+              <input type="text" id="lastName" name="lastName" />
+            </li>
+            <li>
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" />
+            </li>
+            <li>
+              <label htmlFor="message">Message:</label>
+              <input type="text" id="message" name="message" />
+            </li>
+            <button>Send contact form</button>
+          </ul>
+        </form>
+      }
+    </section>
+  );
 }
