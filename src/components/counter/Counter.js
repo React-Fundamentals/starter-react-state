eeb839034aab32c6c7d07d6b9c0a380f924101d8
kchia <kchia87@gmail.com>
2021-10-20 11:58:06 -0400

    add static Counter component

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,3 +1,17 @@
 export default function Counter() {
-  return <h2>Counter</h2>;
+  return (
+    <section>
+      <h1>John's Counter App</h1>
+      <h2>Your current count: 2</h2>
+      <h3>Your current Location: NYC</h3>
+      <p>
+        When you define a component, a common pattern is for an event handler to
+        be a function in the component.
+      </p>
+      <button>increment (+)</button>
+      <button>decrement (-)</button>
+      <button>reset</button>
+      <button>toggle</button>
+    </section>
+  );
 }
