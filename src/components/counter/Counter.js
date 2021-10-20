65f32153036cf2041718c6ba40bcccda457a37c6
kchia <kchia87@gmail.com>
2021-10-20 13:07:37 -0400

    add toggle and counter logic

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import styles from "./Counter.module.css";
 
 export default function Counter() {
   let [count, setCount] = useState(2);
+  const [person, setPerson] = useState({ name: "Jane", location: "NYC" });
+
+  console.log("re-rendering");
 
   function handleIncrement() {
     // count = count + 1; // you can't mutate state directly
@@ -12,14 +16,32 @@ export default function Counter() {
   }
 
   function handleDecrement() {
-    setCount((previousCount) => previousCount - 1);
+    setCount((previousCount) => {
+      return previousCount > 0 ? previousCount - 1 : previousCount;
+    });
   }
+
+  function handleToggle() {
+    setPerson(({ name }) => {
+      return name === "Jane"
+        ? {
+            name: "John",
+            location: "SF",
+          }
+        : {
+            name: "Jane",
+            location: "NYC",
+          };
+    });
+  }
+
+  const counterClass = count > 10 ? styles.red : null;
 
   return (
     <section>
-      <h1>John's Counter App</h1>
-      <h2>Your current count: {count}</h2>
-      <h3>Your current Location: NYC</h3>
+      <h1>{person.name}'s Counter App</h1>
+      <h2 className={counterClass}>Your current count: {count}</h2>
+      <h3>Your current Location: {person.location}</h3>
       <p>
         When you define a component, a common pattern is for an event handler to
         be a function in the component.
@@ -27,7 +49,7 @@ export default function Counter() {
       <button onClick={handleIncrement}>increment (+)</button>
       <button onClick={handleDecrement}>decrement (-)</button>
       <button>reset</button>
-      <button>toggle</button>
+      <button onClick={handleToggle}>toggle</button>
     </section>
   );
 }
