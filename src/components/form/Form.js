99f20c2996cf3321be4a2554ec80a7ee8989c49d
kchia <kchia87@gmail.com>
2021-10-20 18:00:20 -0400

    add form code

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,3 +1,58 @@
-export default function Form() {
-  return <h2>Form</h2>;
+import { useState } from "react";
+
+export default function Form({
+  // firstName = "",
+  // lastName = "",
+  // email = "",
+  // message = "",
+  initialFormData = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: "",
+  },
+}) {
+  const [formData, setFormData] = useState({ ...initialFormData });
+
+  function handleFirstNameChange(event) {
+    setFormData((data) => {
+      return {
+        ...data,
+        firstName: event.target.value,
+      };
+    });
+  }
+
+  return (
+    <section>
+      <h2>Contact Form from Hou Chia</h2>
+      <form>
+        <ul>
+          <li>
+            <label htmlFor="firstName">First Name:</label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleFirstNameChange}
+            />
+          </li>
+          <li>
+            <label htmlFor="lastName">Last Name:</label>
+            <input type="text" id="lastName" name="lastName" />
+          </li>
+          <li>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </li>
+          <li>
+            <label htmlFor="message">Message:</label>
+            <input type="text" id="message" name="message" />
+          </li>
+          <button>Send contact form</button>
+        </ul>
+      </form>
+    </section>
+  );
 }
